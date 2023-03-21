67e4e55306e25567d6ce4e5ffad567e64b0b0560
Samuel Brian Winter <winterss@ad.augsburg.edu>
2023-03-21 10:56:33 -0500

    Updated flocking code

---
+++ Circle.java
@@ -0,0 +1,219 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.util.Vector;
+import java.util.Random;
+import java.util.List;
+
+/**
+ * Circle for drawing in a JFrame
+ *
+ * @author Amy Larson
+ */
+public class Circle extends JPanel {
+
+    /** Unique id (for debugging) */
+    static int nextId = 0;
+
+    static int getId() {
+        return nextId++;
+    }
+
+    private int id;
+
+    /** x and y bounds to keep circles in the playAreas */
+    private final int xMINRANGE = 60;
+    private final int xMAXRANGE = 1090;
+    private final int yMINRANGE = 160;
+    private final int yMAXRANGE = 740;
+
+    /** Fixed size */
+    private int radius = 15;
+
+    /** Color specified in RGB */
+    private Color color = new Color(10, 10, 10);
+
+    /** Location of the JPanel in which the circle is drawn */
+    private Point xy = new Point(0, 0);
+
+    /** Delta of location at each timestep */
+    private Point direction = new Point(+1, +1);
+
+    /** Circels have many random components */
+    private Random random = new Random();
+
+    /** Drawn in window when visible */
+    private boolean visible = false;
+
+    /** Reassigns member variables to the circle. */
+    public void reset() {
+        randomXY();
+        randomDirection();
+        randomColor();
+        setLocation(xy.x, xy.y);
+        showCircle();
+    }
+
+    /** Circle is visible */
+    public void showCircle() {
+        visible = true;
+    }
+
+    /** Circle is not visible */
+    public void hideCircle() {
+        visible = false;
+    }
+
+    public boolean visible(){
+        return visible;
+    }
+
+    /** Default constructor */
+    public Circle() {
+        id = getId(); // for debugging
+
+        this.setSize(radius, radius);
+
+        // Make the box/panel on which the circle is drawn transparent
+        this.setBackground(new Color(0.0f, 0.0f, 0.0f, 0.0f));
+
+        // Randomly assign values
+        randomXY();
+        randomDirection();
+        randomColor();
+    }
+
+    /** Randomly assign its location based on the fixed ranges. */
+    public void randomXY() {
+        // place at random location
+        xy.x = random.nextInt(xMAXRANGE - xMINRANGE) + xMINRANGE;
+        xy.y = random.nextInt(yMAXRANGE - yMINRANGE) + yMINRANGE;
+    }
+
+    /** Randomly point it in a direction with random "speed" */
+    public void randomDirection() {
+        // set in a random direction
+        direction.x = random.nextInt(6) - 3;
+        direction.y = random.nextInt(6) - 3;
+    }
+
+    /** Randomly assign the RGB components */
+    public void randomColor() {
+        // color randomly
+        color = new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    /** Move the robot the "delta" for 1 timestep */
+    /** Move the robot the "delta" for 1 timestep */
+    public void step(List<Circle> circles) {
+        Point separation = new Point(0, 0);
+        Point alignment = new Point(0, 0);
+        Point cohesion = new Point(0, 0);
+        int count = 0;
+
+        for (Circle circle : circles) {
+            if (circle == this || !circle.visible()) {
+                continue;
+            }
+
+            int distance = (int) Math.sqrt(Math.pow(xy.x - circle.xy.x, 2) + Math.pow(xy.y - circle.xy.y, 2));
+            if (distance < radius + circle.radius) {
+                separation.x -= (circle.xy.x - xy.x);
+                separation.y -= (circle.xy.y - xy.y);
+                count++;
+            }
+
+            alignment.x += circle.direction.x;
+            alignment.y += circle.direction.y;
+
+            cohesion.x += circle.xy.x;
+            cohesion.y += circle.xy.y;
+            count++;
+        }
+        if (count > 0) {
+            separation.x /= count;
+            separation.y /= count;
+            separation.x *= -1;
+            separation.y *= -1;
+
+            alignment.x /= count;
+            alignment.y /= count;
+
+            cohesion.x /= count;
+            cohesion.y /= count;
+            cohesion.x = (cohesion.x - xy.x) / 100;
+            cohesion.y = (cohesion.y - xy.y) / 100;
+        }
+
+        direction.x += separation.x + alignment.x + cohesion.x;
+        direction.y += separation.y + alignment.y + cohesion.y;
+
+        xy.x += direction.x;
+        xy.y += direction.y;
+
+        if (xy.x < xMINRANGE || xy.x > yMAXRANGE) {
+            direction.x *= -1;
+        }
+        if (xy.y < yMINRANGE || xy.y > yMAXRANGE) {
+            direction.y *= -1;
+        }
+
+    }
+
+    public boolean overlaps(Circle other) {
+        int distance = (int) Math.sqrt(Math.pow(xy.x - other.xy.x, 2) + Math.pow(xy.y - other.xy.y, 2));
+        return distance < radius + other.radius;
+    }
+
+    public Point getXY() {
+        return xy;
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        // This is called every time the circle location is reset in the CircleModel
+        // System.out.print(" P"+id);
+        super.paintComponent(g);
+        if (visible) {
+            g.setColor(color);
+            g.fillOval(0, 0, radius, radius);
+        }
+    }
+    public Vector<Double> averagePosition(List<Circle> circles) {
+        int count = 0;
+        double averageX = 0.0;
+        double averageY = 0.0;
+
+        // Init vector with fixed size
+        Vector<Double> sum = new Vector<>(2);
+        // sum.add(1, 0.0);
+        // sum.add(2, 0.0);
+        sum.add(0, 0.0);
+        sum.add(1, 0.0);
+        for (Circle circle : circles) {
+            if (circle.visible()) {
+                averageX += circle.getXY().x;
+                averageY = circle.getXY().y;
+                count++;
+            }
+        }
+        if (count > 0) {
+            double averageCircleX = averageX / count;
+            double averageCircleY = averageY / count;
+            sum.add(0, averageCircleX);
+            sum.add(1, averageCircleY);
+            // return sum.get(1,2);
+        }
+        return sum;
+    }
+
+    public Point getDirection() {
+        return direction;
+    }
+    
+    public void setDirection(Point newDirection) {
+        direction = newDirection;
+    }
+}
+
---
+++ CircleModel.java
@@ -0,0 +1,197 @@
+/*
+ * Circle Model.java
+ */
+
+import java.util.ArrayList;
+import java.lang.Thread;
+
+/**
+ * Models a collection of circles roaming about impacting other circles.
+ * 
+ * @author Amy Larson (with Erik Steinmetz)
+ */
+public class CircleModel extends Thread {
+
+    private ArrayList<Circle> circles = new ArrayList<>();
+
+    /** Time in ms. "Frame rate" for redrawing the circles. */
+    private int stepSize = 200;
+    /** Current number of circles visible in the window. */
+    private int count = 0;
+    /** Pauses simulation so circles do not move */
+    private boolean paused = true;
+    /** Number of circles created (visible or not). */
+    private final int numCircles = 100;
+    /**
+     * Strength of cohesion; ie the influence of average direction (between 0 & 1)
+     */
+    private double cohesionStrength = 0.5;
+
+    private SimulationGUI simulation;
+
+    /** Default constructor. */
+    public CircleModel() {
+        // All circels that might appear in the graphics window are created, but are not
+        // visible.
+        for (int i = 0; i < numCircles; i++) {
+            circles.add(new Circle());
+        }
+    }
+
+    public void setSim(SimulationGUI sim) {
+        simulation = sim;
+    }
+
+    @Override
+    public void run() {
+        // Forever run the simulation
+        while (true) {
+            // Move things only if the simulation is not paused
+            if (!paused) {
+                advanceCircles();
+                simulation.getContentPane().repaint();
+            }
+            try {
+                Thread.sleep(stepSize);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    /** Pause the simulation - circles freeze. */
+    public void pause() {
+        paused = true;
+    }
+
+    /** Circles move again */
+    public void play() {
+        System.out.println("Playing now");
+        paused = false;
+    }
+
+    /** Move circles to next location */
+    public void advanceCircles() {
+        for (int i = 0; i < count; i++) {
+            // Advance each circle
+            circles.get(i).step(circles);
+
+            // Check for collision with other circles
+            for (int j = i + 1; j < count; j++) {
+                if (circles.get(i).overlaps(circles.get(j))) {
+                    handleCollision(circles.get(i), circles.get(j));
+                }
+            }
+            // Set the location, which prompts the viewer to newly display the circle
+            circles.get(i).setLocation(circles.get(i).getXY().x, circles.get(i).getXY().y);
+        }
+    }
+    /** Handle collision between two circles */
+    private void handleCollision(Circle c1, Circle c2) {
+        // Get current velocity and position for each circle
+        Point v1 = c1.getDirection();
+        Point v2 = c2.getDirection();
+        Point p1 = c1.getXY();
+        Point p2 = c2.getXY();
+
+        // Calculate new velocity for each circle after collision
+        Point newV1 = new Point(v2.x, v2.y);
+        Point newV2 = new Point(v1.x, v1.y);
+
+        // Update velocity for each circle
+        c1.setDirection(newV1);
+        c2.setDirection(newV2);
+    }
+
+
+    public ArrayList<Circle> getCircles() {
+        return circles;
+    }
+
+    /**
+     * Modify the direction of each circle toward average, "pulling" them in the
+     * same direction
+     */
+    private void cohesion() {
+        Point difference;
+        Point newDirection;
+        int xModified;
+        int yModified;
+        for (int i = 0; i < count; i++) {
+            difference = calculateDifference(circles.get(i));
+            xModified = ((int) ((cohesionStrength) * difference.x)) + circles.get(i).getDirection().x;
+            yModified = ((int) ((cohesionStrength) * difference.y)) + circles.get(i).getDirection().y;
+            newDirection = new Point(xModified, yModified);
+            circles.get(i).setDirection(newDirection);
+        }
+    }
+
+    /**
+     * Returns a Point with values that represent difference between a circle's
+     * direction and the average
+     * direction
+     */
+    private Point calculateDifference(Circle circle) {
+
+        // Average X and Y values for all visible circle directions
+        Point avgDirection = avgDirection();
+
+        // Difference between the average direction value, and the given direction value
+        int xDifference = avgDirection.x - circle.getDirection().x;
+        int yDifference = avgDirection.y - circle.getDirection().y;
+
+        Point difference = new Point(xDifference, yDifference);
+
+        return difference;
+    }
+
+    // ** Calculates average direction of all visible circles */
+    private Point avgDirection() {
+        int Xsum = 0;
+        int Ysum = 0;
+        int Xavg;
+        int Yavg;
+        Point avgDirection;
+        for (int i = 0; i < count; i++) {
+            Xsum += circles.get(i).getDirection().x;
+            Ysum += circles.get(i).getDirection().y;
+        }
+        Xavg = Xsum / count;
+        Yavg = Ysum / count;
+        avgDirection = new Point(Xavg, Yavg);
+        return avgDirection;
+    }
+
+    /** Reset circles */
+    public void setCount(int circleCount) {
+        System.out.println("Making circles!");
+        // Must be in bounds. Only 40 circles in the list.
+        if (circleCount < 2) {
+            circleCount = 2;
+        } else if (circleCount > numCircles) {
+            circleCount = numCircles;
+        }
+        // Reset "count" circles, making them visible
+        count = circleCount;
+        for (int i = 0; i < count; i++) {
+            circles.get(i).reset();
+        }
+        // Hide the rest
+        for (int i = count; i < numCircles; i++) {
+            circles.get(i).hideCircle();
+        }
+    }
+
+    /** Set speed of simulation from 1 (slow) to 5 (fast) */
+    public void setSpeed(int newSpeed) {
+        // speed is between 1 (slow) and 5 (fastest)
+        // low speed = high step size
+        if (newSpeed < 1) {
+            newSpeed = 1;
+        } else if (newSpeed > 5) {
+            newSpeed = 5;
+        }
+        stepSize = (6 - newSpeed) * 80; // 80 to 400ms
+    }
+
+}
